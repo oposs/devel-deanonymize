5ba140e48ba00254fdade8dd82e0024c9af3659d
Tobias Bossert <tobias.bossert@fastpath.ch>
2021-06-23 12:17:57 +0200

    Made modify_files more robust, added module structure, added doc

--- lib/Devel/Deanonymize.pm
+++ lib/Devel/Deanonymize.pm
@@ -1,20 +1,85 @@
+=head1 NAME
+
+Devel::Deanonymize - A small tool to make anonymous sub visible
+
+=head1 DESCRIPTION
+
+When collecting Coverage statistics with L<Devel::Cover> a construct like below appear to be invisible and is simply ignored
+by the statistic:
+
+    my $sub = sub handle{
+        print "hello";
+    }
+
+This script aims to solve this problem by wrapping each file in a sub and thus making these subs I<visible>.
+
+=head1 SYNOPSIS
+
+    # Perl scripts
+    perl -MDevel::Cover=-ignore,^t/,Deanonymize -MDevel::Deanonymize=<inculde_pattern> your_script.pl
+
+    # Perl tests
+    HARNESS_PERL_SWITCHES="-MDevel::Cover=-ignore,^t/,Deanonymize -MDevel::Deanonymize=<include_pattern"  prove t/
+
+
+
+=head1 EXAMPLES
+
+Please referer to the files provided in the I<examples/> directory
+
+
+=head1 AUTHORS
+
+Since there is a lot of spam flooding my mailbox, I had to put spam filtering in place. If you want to make sure
+that your email gets delivered into my mailbox, include C<#im_not_a_bot#> in the B<subject!>
+
+S<Tobias Bossert E<lt>tobib at cpan.orgE<gt>>
+
+=head1 COPYRIGHT AND LICENSE
+
+MIT License
+
+Copyright (c) 2021 Tobias Bossert, Oetiker + Partner AG Switzerland
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+=cut
+
 package Devel::Deanonymize;
 use strict;
 use warnings FATAL => 'all';
+
+our $VERSION = "0.1.0";
 
 my $include_pattern;
 
 sub import {
     # capture input parameters
-    $include_pattern = $_[1];
+    $include_pattern = $_[1] ? $_[1] : die("Devel::Deanonymize: An include Pattern must be specified \n");
 }
 
-sub modify_files{
-    # Internal note:
+sub modify_files {
+    # Internal notes:
     # Basically, this code replaces every file path in @INC with a reference to an anonymous sub which wraps each
     # file in sub classWrapper { $orig_content } classWrapper(); However, this sub is **not** necessarily run at INIT or UNITCHECK stage!
     # NB, this also explains why its is possible to have $include_pattern "defined" at UNITCHECK even if its run **before** import()
-    # Also do not forget the `__END__` at each file
+    # Also make sure each file either ends with __DATA__, __END__, or 1;
     unshift @INC, sub {
         my (undef, $filename) = @_;
         return () if ($filename !~ /$include_pattern/);
@@ -24,10 +89,15 @@ sub modify_files{
             my $module_text = <$fh>;
             close $fh;
 
+            if (not $module_text =~ /(__END__|1;|__DATA__)/) {
+                warn("Devel::Deanonymize: Found no endmarker in file `$filename` - skipping\n");
+                return ();
+            }
+
             # define everything in a sub, so Devel::Cover will DTRT
             # NB this introduces no extra linefeeds so D::C's line numbers
             # in reports match the file on disk
-            $module_text =~ s/(.*?package\s+\S+)(.*)__END__/$1sub classWrapper {$2} classWrapper();/s;
+            $module_text =~ s/(.*?package\s+\S+)(.*)(__END__|1;|__DATA__)/$1sub classWrapper {$2} classWrapper();/s;
 
             # unhide private methods to avoid "Variable will not stay shared"
             # warnings that appear due to change of applicable scoping rules
@@ -51,9 +121,9 @@ sub modify_files{
 
 
 # We call modify_files twice since depending on how a module is loaded (use or required) it is present in @INC at different stages
-# A "double-modification" is not possible because we only edit non references
+# Also, "double-modification" is not possible because we only alter non references
 INIT {
-   modify_files();
+    modify_files();
 }
 
 UNITCHECK {
